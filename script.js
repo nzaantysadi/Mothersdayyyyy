54aa06b980d932e0391b61f62e5138d876261d27
Theo <148655093+nzaantysadi@users.noreply.github.com>
2025-05-10 22:04:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById("startBtn").addEventListener("click", () => {
-  document.getElementById("sky").style.display = "block";
-  document.getElementById("startBtn").style.display = "none";
-});
